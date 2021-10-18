05277e2cc777e3d9fe47fe14e5098ae17d1e046b
duchangkim <duchang.dev@gmail.com>
2021-10-18 22:06:50 +0900

    feat: Add examples

--- src/dev.ts
+++ src/dev.ts
@@ -3,3 +3,24 @@ import Timer from './core/Timer';
 const devSpan = document.getElementById('dev') as HTMLElement;
 
 devSpan.innerHTML = '!!DEV!!';
+
+const countdownTimer = new Timer({
+  type: 'countdown',
+  seconds: 5,
+});
+
+const countdownStartButton = document.getElementById(
+  'countdownButton',
+) as HTMLElement;
+const countdownSpace = document.getElementById('countdownSpace') as HTMLElement;
+
+countdownStartButton.addEventListener('click', () => {
+  countdownTimer.start();
+});
+
+const updateDOM = () => {
+  console.log('얼마나 이벤트 발생하는지?');
+  countdownSpace.innerHTML = countdownTimer.getSeconds().toString();
+};
+
+countdownTimer.addEventListener('seconds-updated', updateDOM);
