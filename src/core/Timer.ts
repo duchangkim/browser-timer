8f5e5b360a749fd732a688ff102676f4353852c5
duchangkim <duchang.dev@gmail.com>
2021-10-25 08:03:41 +0900

    feat: Improve timer methods
    start, pause, stop, reset...

--- src/core/Timer.ts
+++ src/core/Timer.ts
@@ -8,13 +8,15 @@ export interface TimerOptions {
 }
 
 export default class Timer {
-  eventBus: EventBus;
+  private eventBus: EventBus;
+  private intervalRef = 0;
+  private isPause = false;
   type: TimerType;
   seconds: number;
   msCount = 0;
   endTime = 0;
-  intervalRef = 0;
   timerSeconds = 0;
+  timerMilliseconds = 0;
 
   constructor(options: TimerOptions) {
     this.eventBus = new EventBus();
@@ -26,18 +28,53 @@ export default class Timer {
   start() {
     if (this.intervalRef > 0) return;
 
-    this.endTime = Date.now() + this.seconds * 1000;
+    console.log('start!');
+
+    if (!this.isPause) {
+      this.msCount = this.seconds * 1000;
+    }
+
+    this.endTime = Date.now() + this.msCount;
     this.intervalRef = window.setInterval(
       () => (this.type === 'countdown' ? this.countdown() : this.timer()),
-      10,
+      1,
     );
+    this.isPause = false;
   }
 
-  pause() {}
+  pause() {
+    if (this.intervalRef === 0) return;
 
-  restart() {}
+    console.log('pause!');
 
-  reset() {}
+    clearInterval(this.intervalRef);
+    this.intervalRef = 0;
+    this.isPause = true;
+    this.eventBus.emit('pause');
+  }
+
+  stop() {
+    if (this.intervalRef === 0) return;
+
+    console.log('stop!');
+
+    clearInterval(this.intervalRef);
+    this.intervalRef = 0;
+    this.eventBus.emit('stop');
+  }
+
+  reset(second?: number) {
+    console.log('reset!');
+
+    if (second) {
+      this.seconds = second;
+    }
+    this.msCount = this.seconds * 1000;
+    this.endTime = Date.now() + this.msCount;
+    this.timerSeconds = this.seconds;
+    this.eventBus.emit('secondsUpdated');
+    this.eventBus.emit('reset');
+  }
 
   setSeconds(seconds: number) {
     this.seconds = seconds;
@@ -47,17 +84,28 @@ export default class Timer {
     const currentTime = Date.now();
     this.msCount = this.endTime - currentTime;
 
-    const s = Math.ceil((this.msCount / 1000) % 60);
+    if (this.msCount <= 0) {
+      this.timerSeconds = 0;
+      this.eventBus.emit('secondsUpdated');
+      this.timerMilliseconds = 0;
+      console.log(this.msCount, 'when finish');
+
+      window.clearInterval(this.intervalRef);
+      this.intervalRef = 0;
+      this.eventBus.emit('finish');
+    }
+
+    const ms = this.msCount <= 0 ? 0 : this.msCount % 1000;
+    if (this.timerMilliseconds !== ms) {
+      this.timerMilliseconds = ms;
+      this.eventBus.emit('millisecondsUpdated');
+    }
+
+    const s = this.msCount <= 0 ? 0 : Math.floor(this.msCount / 1000);
 
     if (this.timerSeconds !== s) {
       this.timerSeconds = s;
-      this.eventBus.emit('seconds-updated');
-    }
-
-    if (this.msCount < 0) {
-      window.clearInterval(this.intervalRef);
-      this.intervalRef = 0;
-      this.eventBus.emit('timer-finish');
+      this.eventBus.emit('secondsUpdated');
     }
   }
 
@@ -66,6 +114,17 @@ export default class Timer {
   // getters
   getSeconds() {
     return this.timerSeconds;
+  }
+
+  // 1/1000th of a second -> 1000ms === 1s
+  getMilliseconds(digit?: number): number {
+    if (digit === 1) {
+      return Math.floor(this.timerMilliseconds / 100);
+    } else if (digit === 2) {
+      return Math.floor(this.timerMilliseconds / 10);
+    }
+
+    return this.timerMilliseconds;
   }
 
   // events
