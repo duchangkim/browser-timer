5d07ea6b35748ec30c411b788395ce298e5acdb8
duchangkim <duchang.dev@gmail.com>
2021-10-25 08:04:32 +0900

    feat: Improve timer demo

--- src/dev.ts
+++ src/dev.ts
@@ -4,23 +4,52 @@ const devSpan = document.getElementById('dev') as HTMLElement;
 
 devSpan.innerHTML = '!!DEV!!';
 
+// countdown area
 const countdownTimer = new Timer({
   type: 'countdown',
   seconds: 5,
 });
 
-const countdownStartButton = document.getElementById(
-  'countdownButton',
-) as HTMLElement;
-const countdownSpace = document.getElementById('countdownSpace') as HTMLElement;
+const countdownStartButton = document.getElementById('countdownButton') as HTMLElement;
+const countdownPauseButton = document.getElementById('countdownPause') as HTMLElement;
+const countdownStopButton = document.getElementById('countdownStop') as HTMLElement;
+const countdownResetButton = document.getElementById('countdownReset') as HTMLElement;
+const sec = document.getElementById('sec') as HTMLSpanElement;
+const ms = document.getElementById('ms') as HTMLSpanElement;
 
 countdownStartButton.addEventListener('click', () => {
+  const second =
+    parseInt((document.getElementById('countdownInput') as HTMLInputElement).value) ||
+    countdownTimer.getSeconds();
+  countdownTimer.setSeconds(second);
   countdownTimer.start();
+});
+countdownPauseButton.addEventListener('click', () => {
+  countdownTimer.pause();
+});
+countdownStopButton.addEventListener('click', () => {
+  countdownTimer.stop();
+});
+countdownResetButton.addEventListener('click', () => {
+  const second =
+    parseInt((document.getElementById('countdownInput') as HTMLInputElement).value) ||
+    countdownTimer.getSeconds();
+  countdownTimer.reset(second);
 });
 
 const updateDOM = () => {
   console.log('얼마나 이벤트 발생하는지?');
-  countdownSpace.innerHTML = countdownTimer.getSeconds().toString();
+  sec.innerText = countdownTimer.getSeconds().toString();
 };
 
-countdownTimer.addEventListener('seconds-updated', updateDOM);
+countdownTimer.addEventListener('finish', () => {
+  (document.getElementById('done') as HTMLDivElement).innerText = countdownTimer
+    .getSeconds()
+    .toString();
+});
+countdownTimer.addEventListener('secondsUpdated', updateDOM);
+countdownTimer.addEventListener('millisecondsUpdated', () => {
+  // console.log(countdownTimer.getMilliseconds(2).toString());
+
+  ms.innerText = countdownTimer.getMilliseconds(2).toString();
+});
